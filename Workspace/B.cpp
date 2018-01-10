ad3c7ef1550fd0ab246ec1aec28e8b3ae0f9ef6e
zhangche0526 <zhangche0526@163.com>
2018-01-10 22:02:44 +0800

    2018-01-10 22:02:44

--- Workspace/B.cpp
+++ Workspace/B.cpp
@@ -7,23 +7,35 @@ const int P=998244353;
 
 ll n;int lgn;
 
-std::map<ll,int> f[2],g[2];
-
-int dp(bool c,ll len)
+struct S{ll x,y;S(ll x=0,ll y=0):x(x),y(y){}};
+S operator +(S a,S b)
 {
-	if(f[c][len]) return f[c][len];
-	int rL=len>>1,lL=len-rL;
-	if(c) f[c][len]=(ll)f[0][lL]*f[1][rL]%P+(ll)f[1][lL]*f[0][rL]%P;
-	else f[c][len]=(ll)f[1][lL]*f[1][rL]%P;
-	return f[c][len];
+	if(a.x>b.x) return a;
+	if(a.x<b.x) return b;
+	return S(a.x,(a.y+b.y)%P);
+}
+S operator *(S a,S b){return S(a.x+b.x,a.y*b.y%P);}
+struct P{S s0,s1;P(){}P(S s0,S s1):s0(s0),s1(s1){}};
+
+std::map<ll,P> f;
+
+P dp(ll l,ll r)
+{
+	ll len=r-l+1;
+	if(f.count(len)) return f[len];
+	if(l==r) return f[len]=P(S(0,1),S(-1,0));
+	ll mid=(l+r)>>1;
+	P l=dp(l,mid),r=dp(mid+1,r);
+	S s0=(l.s0+l.s1)*(r.s0+r.s1),
+	  s1=(l.s0*(r.s0+r.s1))+(r.s0*(l.s0+l.s1));
+	if(s1.x>=0) s1.x++;
+	return f[len]=P(s0,s1);
 }
 
 int main()
 {
-	int i;
 	scanf("%lld",&n);
-	f[0][n]=f[1][n]=1;
-	int ans=(dp(0,n)+dp(1,n))%P;
-	printf("%d",ans);
+	P ans=dp(1,n);ans.s0=ans.s0+ans.s1;
+	printf("%lld %lld",ans.s0.x,ans.s0.y);
 	return 0;
 }
